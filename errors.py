987c530729ca5ffdeefb23a81abced931fcc9a94
premedi92 <70114828+premedi92@users.noreply.github.com>
2022-12-19 11:06:32 -0500

    Update and rename errores.py to errors.py
    Cambio de la funcion para imprimir errores en pantalla a partir de un codigo de error que recibe.

---
+++ errors.py
@@ -0,0 +1,9 @@
+def send_message(error_code):
+   if error_code == 1:
+      print("Ticker is invalid, please verify and enter again.")
+   elif error_code == 2:
+      print("Dates are invalid, please verify and enter again.")
+   elif error_code == 3:
+      print("The range requested exceeds the capabilities of this software, please enter a shorter date range.")
+   elif error_code == 4:
+      print("Database cannot be accessed. Please make sure the file exists and is not being used by other processes, then try again.")
